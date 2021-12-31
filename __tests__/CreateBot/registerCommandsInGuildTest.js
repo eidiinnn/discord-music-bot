52cf268b08cb7565debaab539bf60233af8353fd
Eduardo da Silva <67817853+eidiinnn@users.noreply.github.com>
2021-12-31 12:24:39 -0300

    Update registerCommandsInGuildTest.js

--- __tests__/CreateBot/registerCommandsInGuildTest.js
+++ __tests__/CreateBot/registerCommandsInGuildTest.js
@@ -2,6 +2,7 @@ import CreateBot from '../../src/createBot';
 
 require('dotenv').config();
 
+const bot = new CreateBot(process.env.BOT_API_TOKEN, process.env.CLIENT_ID);
 const commands = [
   {
     name: 'ping',
@@ -9,26 +10,22 @@ const commands = [
   },
 ];
 
-test('registerCommandsInGuild works', () => {
-  const bot = new CreateBot(process.env.BOT_API_TOKEN, process.env.CLIENT_ID);
-  return bot
+test('registerCommandsInGuild works', () =>
+  bot
     .registerCommandsInGuild(process.env.GUILD_ID, commands)
     .then((returnedBoolean) => {
       expect(returnedBoolean).toBe(true);
     })
     .catch((error) => {
       throw new Error(error);
-    });
-});
+    }));
 
-test('registerCommandsInGuild detects an error', () => {
-  const bot = new CreateBot(process.env.BOT_API_TOKEN, process.env.CLIENT_ID);
-  return bot
+test('registerCommandsInGuild detects an error', () =>
+  bot
     .registerCommandsInGuild(process.env.GUILD_ID, 'wrong-commands')
     .then()
     .catch((error) => {
       expect(() => {
         throw new Error(error);
       }).toThrow();
-    });
-});
+    }));
